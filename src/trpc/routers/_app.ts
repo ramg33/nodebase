b01ca8a771dd1aa846fdbcfc92a58dde7b620f56
Rodrigo Moza <rodrigo.moza24@gmail.com>
2025-12-24 13:50:02 -0600

    06: background jobs

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,8 +1,19 @@
-import { baseProcedure, createTRPCRouter, protectedProcedure } from "../init";
+import { inngest } from "@/inngest/client";
+import { createTRPCRouter, protectedProcedure } from "../init";
 import prisma from "@/lib/db";
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure.query(({ ctx }) => {
-    return prisma.user.findMany();
+  getWorkflows: protectedProcedure.query(({ ctx }) => {
+    return prisma.workflow.findMany();
+  }),
+  createWorkflow: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: "rodrigo.moza@elaniin.com",
+      },
+    });
+
+    return { success: true, message: "Job queued" };
   }),
 });
 // export type definition of API
