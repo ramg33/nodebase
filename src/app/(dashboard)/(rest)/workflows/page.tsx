bdf573026e945b01c9d7f60699cf5ca042bbcfe3
Rodrigo Moza <rodrigo.moza24@gmail.com>
2025-12-26 16:49:36 -0600

    11: workflows crud

--- src/app/(dashboard)/(rest)/workflows/page.tsx
+++ src/app/(dashboard)/(rest)/workflows/page.tsx
@@ -1,8 +1,27 @@
+import { prefetchWOrkflows } from "@/features/workflows/server/prefetch";
 import { requiredAuth } from "@/lib/auth-utils";
+import { HydrateClient } from "@/trpc/server";
+import { ErrorBoundary } from "react-error-boundary";
+import { Suspense } from "react";
+import {
+  WorkflowsContainer,
+  WorkflowsList,
+} from "@/features/workflows/components/workflows";
 
 const Page = async () => {
   await requiredAuth();
-  return <p>Workflows</p>;
+  prefetchWOrkflows();
+  return (
+    <WorkflowsContainer>
+      <HydrateClient>
+        <ErrorBoundary fallback={<p>Error!</p>}>
+          <Suspense fallback={<p>Loading...</p>}>
+            <WorkflowsList />
+          </Suspense>
+        </ErrorBoundary>
+      </HydrateClient>
+    </WorkflowsContainer>
+  );
 };
 
 export default Page;
